de27ad088f5c8dbeae55df93e532447d124cf30f
cbrlpdr <cbrl.pdr@gmail.com>
2023-04-28 17:55:13 +0000

    Adding imports and creating Notepad class

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter
+import os
+from tkinter import*
+from tkinter.messagebox import*
+from tkinter.filedialog import*
+
+class Notepad:
+    __root = Tk()
+    __thisWidth = 300
+    __thisHeight = 300
+    __thisTextArea = Text(__root)
+    __thisMenuBar = Menu(__root)
+    __thisFileMenu = Menu(__thisMenuBar, tearoff=0)
+    __thisEditMenu = Menu(__thisMenuBar, tearoff=0)
+    __thisHelpMenu = Menu(__thisMenuBar, tearoff=0)
+
+    #Scrollbar
+    __thisScrollBar = Scrollbar(__thisTextArea)
+
+    __file = None
